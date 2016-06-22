768a901f929e34eeca23ce88da293c46d2404c88
IronyGames <ironygames@gmail.com>
2016-06-22 19:06:52 +0200

    Implement typing and color changing (#1)
    * -removed unnecessary metafiles
    
    * -moved
    
    * - Added PlayerColorManager
    - Added PlayerSpellchecker
    - Implemented game functionality

---
+++ World's smallest Hackmachine! (2016 1 Bit Jam IronyGames)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	private MeshRenderer thisMesh;
+	private PlayerSpellchecker spellManager;
+
+	void Start ()
+	{
+		spellManager = GetComponent<PlayerSpellchecker> ();
+		spellManager.reset ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetKey ("escape")) {
+			Application.Quit ();
+		}
+		if (!spellManager.wasListCompleted ()) {
+			spellManager.asyncUpdate ();
+		}
+
+	}
+}
+
